6b7067a148048394c0cc068b56068480b0247055
Alexpilha <alex_pilha@hotmail.com>
2020-07-16 19:02:21 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,13 +7,26 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
+	
+	 private int turn;
+	 private Color currentPlayer;
 
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
+	
+	 public int getTurn() {
+		 return turn;
+	 }
+	 
+	   public Color getCurrentPlayer() {
+		   return currentPlayer;
+	   }
 	
 	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -38,9 +51,10 @@ public class ChessMatch {
 		    Position target = targetPosition.toPosition();
 		      ValidateSourcePosition(source);
 		        ValidateTargetPosition(source, target);
-		        Piece capturedPiece = makeMove(source, target);
-		         return (ChessPiece) capturedPiece;		        
-		      
+		         Piece capturedPiece = makeMove(source, target);
+		          nextTurn();
+		           return (ChessPiece) capturedPiece;		        
+		        
 	}
 	
 	   private Piece makeMove(Position source, Position target) {
@@ -55,6 +69,9 @@ public class ChessMatch {
 	private void ValidateSourcePosition(Position position) {
 		 if (!board.thereIsAPiece(position)) {
 			 throw new ChessException("there is no piece on source position");
+		 }
+		 if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			 throw new ChessException("the chosen piece is not yours");
 		 }
 		 
 		 if (!board.piece(position).isThereAnyPossibleMove()) {
@@ -71,6 +88,11 @@ public class ChessMatch {
 		  if (!board.piece(source).possibleMove(target)) {
 			  throw new ChessException("the chosen piece can't move to target position");
 		  }
+	  }
+	  
+	  private void nextTurn() {
+		  turn++;
+		  currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	  }
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
